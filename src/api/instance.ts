5b27607b8d4bdb3e78bb81028724f3c595e12f73
temur <suvonovtemur2008@gmail.com>
2025-03-13 12:25:22 +0500

    changing data and base url

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://test.argos.uz/api",
+  baseURL: "https://demo-test.argos.uz/api",
   headers: {
     "Content-Type": "application/json",
   },
