40c3a0335114d7b1da0272ad7c85ff5fa28eca6c
temur <suvonovtemur2008@gmail.com>
2025-03-28 10:09:05 +0500

    pushing new changes

---
+++ src/components/Alert.tsx
@@ -0,0 +1,10 @@
+export const AlertComponent = () => {
+  return (
+    <div className="p-3 bg-yellow-50 border-l-4 border-yellow-500 text-yellow-700 rounded-r">
+      <div>
+        <p className="font-medium">Saytda texnik ishlar olib borilmoqda</p>
+        <p className="mt-1">Keltirilgan noqulayliklar uchun uzr so'raymiz!</p>
+      </div>
+    </div>
+  );
+};
